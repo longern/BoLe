63bdcecc1a6ded264e3fc1b58f4ea5e83b71732d
Siyu Long <771815005@qq.com>
2016-05-14 10:16:42 +0800

    Add drawNote function

--- bole.js
+++ bole.js
@@ -3,11 +3,14 @@ navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia 
 
 var audioCtx = new AudioContext();
 var analyser = audioCtx.createAnalyser();
+var ctx = null;
 analyser.fftSize = 1024;
 
 var msPerBeat = 60000 / 105.;
 var musicScore = [[-3, 0], [-1, 1], [0, 2], [4, 3], [9, 4], [-3, 8], [-1, 9], [0, 10], [4, 11], [9, 12], [14, 16], [12, 17], [11, 18], [12, 19], [9, 20], [-3, 24], [-1, 25], [0, 26], [4, 27], [9, 28], [-3, 32], [-1, 33], [0, 34], [4, 35], [9, 36], [14, 48], [12, 49], [11, 50], [12, 51], [9, 52]];
+var gameScore = 0.;
 
+// Global Functions
 function ellapseTime() {
     var endTime = new Date().getTime();
     return endTime - gameBeginTime;
@@ -15,6 +18,13 @@ function ellapseTime() {
 
 function indexToFrequency(index) {
     return index * audioCtx.sampleRate / analyser.fftSize;
+}
+
+// Render Functions
+function drawNote(x, y) {
+    ctx.beginPath();
+    ctx.ellipse(x, y, 9, 7, -25 * Math.PI / 180, 0, 2 * Math.PI);
+    ctx.fill();
 }
 
 function drawLines() {
@@ -39,15 +49,11 @@ function renderGame() {
 
     drawLines();
 
-    ctx.beginPath();
-    ctx.ellipse(460, 264, 9, 7, -25 * Math.PI / 180, 0, 2 * Math.PI);
-    ctx.fill();
-
-    ctx.beginPath();
-    ctx.ellipse(632, 235, 9, 7, -25 * Math.PI / 180, 0, 2 * Math.PI);
-    ctx.fill();
+    drawNote(460, 264);
+    drawNote(632, 235);
 }
 
+// Logic Functions
 function analyseAudio() {
     bufferLength = analyser.frequencyBinCount;
     fftDataArray = new Float32Array(bufferLength);
@@ -55,6 +61,7 @@ function analyseAudio() {
     document.title = Math.round(indexToFrequency(fftDataArray.indexOf(Math.max.apply(null, fftDataArray)))) + " Hz";
 }
 
+// Main Loop
 function timerEvent() {
     analyseAudio();
     renderGame();
